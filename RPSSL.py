6d3a7f3c4204348ade39904458f27188ceb94139
MatteoKastler <matteo.kastler@gmail.com>
2022-11-16 10:31:02 +0100

    rpssl vs computer final

--- RPSSL.py
+++ RPSSL.py
@@ -1,3 +1,4 @@
+import random
 
 symbol = ["Rock", "Lizard", "Spock", "Scissors", "Paper"]
 #1 schlägt 2 usw
@@ -5,19 +6,23 @@ def getWinner(a, b):
     if(a == b):return 0
     if((a+1)%(len(symbol)) == b): return 1
     if((a+3)%(len(symbol)) == b): return 1
-    return 2
+    return
 
-player = -1
 if __name__ == '__main__':
+    player = -1
     for i in range(len(symbol)):
         print(f'{i}: {symbol[i]}')
     player = int(input("Please choose a weapon"))
     while(player == -1 or type(player) != int or player > 4):
         player = int(input("try again"))
-    result = getWinner(player, 0)
+
+    comp = random.randint(0,len(symbol)-1)
+    result = getWinner(player, comp)
+    print(f'\nPlayer choice: {symbol[player]}')
+    print(f'Computer choice {symbol[comp]}')
     if(result == 1):
-        print("Du hast gewonnen")
+        print("You won")
     elif(result == 2):
-        print("Computer hat gewonnen")
+        print("Computer won")
     else:
-        print("Gleiche Waffe gewählt")
+        print("Draw")
